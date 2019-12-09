8aeb6f4d3682ce3bb819d9863e8b91aba64820c4
ChaitanyaAkula <krishchaituakula@gmail.com>
2019-12-09 17:50:38 +0530

    Add files via upload

---
+++ clientprofileupload.go
@@ -0,0 +1,36 @@
+package clientprofileupload
+import(
+	"fmt"
+	"net/http"
+	"os"
+	"io"
+	"path/filepath"
+	"crypto/sha1"
+	"strings"
+    "mime/multipart"
+)
+
+
+
+func SetProfile(w http.ResponseWriter,mf multipart.File,fh *multipart.FileHeader)string{
+
+	defer mf.Close()
+	ext := strings.Split(fh.Filename, ".")[1]
+	h := sha1.New()
+	io.Copy(h, mf)
+	fname := fmt.Sprintf("%x", h.Sum(nil)) + "." + ext
+	wd, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+	}
+	path := filepath.Join(wd, "public", "clientprofile", fname)
+	nf, err := os.Create(path)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer nf.Close()
+	mf.Seek(0, 0)
+	io.Copy(nf, mf)
+	
+	return fname
+}
